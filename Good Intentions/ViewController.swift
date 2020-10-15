866b3d806f284a126601958ef9c39c10092abbe2
Sean Johnson <sean@snootymonkey.com>
2020-10-15 08:05:21 -0400

    Bold times in intention message. Color action buttons. Show the current time.

--- Good Intentions/ViewController.swift
+++ Good Intentions/ViewController.swift
@@ -9,15 +9,75 @@
 import Darwin
 import Cocoa
 
+// Future config:
+// Launch app's name
+// Time slots (begin, end)
+// App time out
+
 class ViewController: NSViewController {
 
     @IBOutlet weak var quitButton: NSButton!
+    @IBOutlet weak var quitButtonCell: NSButtonCell!
     @IBOutlet weak var launchButton: NSButton!
-
+    @IBOutlet weak var launchButtonCell: NSButtonCell!
+    @IBOutlet weak var intentMsgField: NSTextField!
+    @IBOutlet weak var nowField: NSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Set action button colors
+        
+        // CSS #265500
+        quitButtonCell.backgroundColor = NSColor.init(red: 0.149019608,
+                                                      green: 0.333333333,
+                                                      blue: 0.0,
+                                                      alpha: 1.0)
+        // CSS #b7241b
+        launchButtonCell.backgroundColor = NSColor.init(red: 0.717647059,
+                                                        green: 0.141176471,
+                                                        blue: 0.105882353,
+                                                        alpha: 1.0)
+
+        // Get the current time
+        let now = Date()
+        print(now)
+
+        // Immediately launch the app if in the time window
+     
+        // let calendar = Calendar.current
+        // let hour = calendar.component(.hour, from: now)
+        // let minutes = calendar.component(.minute, from: now)
+
+        // Set the current time in the UI
+        let formatter = DateFormatter()
+        formatter.dateStyle = .none
+        formatter.timeStyle = .short
+        nowField.stringValue = formatter.string(from: now)
+        
+        // Set the intent message in the UI
+        let intentMsg = "Your intent is to check email at 8:30, 11:30 and 15:30."
+        let font = NSFont.systemFont(ofSize: 12)
+        let boldFont = NSFont.boldSystemFont(ofSize: 12)
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .center
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font: font,
+            .foregroundColor: NSColor.white,
+            .paragraphStyle: paragraphStyle
+        ]
+        let boldAttributes: [NSAttributedString.Key: Any] = [
+            .font: boldFont,
+            .foregroundColor: NSColor.white,
+            .paragraphStyle: paragraphStyle
+        ]
+        let intentMsgString = NSMutableAttributedString(string: intentMsg,attributes: attributes)
+        intentMsgString.setAttributes(boldAttributes,range: NSRange(location: 33, length: 4))
+        intentMsgString.setAttributes(boldAttributes,range: NSRange(location: 40, length: 4))
+        intentMsgString.setAttributes(boldAttributes,range: NSRange(location: 50, length: 4))
+        intentMsgField.attributedStringValue = intentMsgString
+
+        // Start the app timer
     }
 
     override var representedObject: Any? {
@@ -31,8 +91,8 @@ class ViewController: NSViewController {
     }
 
     @IBAction func launch(sender: NSButton) {
+        // On macOS 10.15+ the following is preferred
         // let appURL = URL(fileURLWithPath: "/Applications/MailMate.app")
-        // 10.15+
         // NSWorkspace.shared.openApplication(at: appURL,
         //                                    configuration: <#T##NSWorkspace.OpenConfiguration#>,
         //                                    completionHandler: <#T##((NSRunningApplication?, Error?) -> Void)?##((NSRunningApplication?, Error?) -> Void)?##(NSRunningApplication?, Error?) -> Void#>)
@@ -41,4 +101,3 @@ class ViewController: NSViewController {
     }
 
 }
-
